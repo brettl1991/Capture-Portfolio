9c0253826ab24603e99f25f9fdbf56722b837fde
brettl1991 <brettl1991@gmail.com>
2022-08-09 18:34:45 +0100

    Faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions <span>FAQ</span>
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Sapiente, quo.
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Sapiente, quo.
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Different Payment Methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Sapiente, quo.
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>What Products Do You Offer?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Sapiente, quo.
+            </p>
+          </div>
+        </div>
+      </h2>
+    </div>
+  );
+};
+
+export default FaqSection;
