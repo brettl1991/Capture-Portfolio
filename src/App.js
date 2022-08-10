30f958e4785bba1f46facdee2463dbdd3a61e9f1
brettl1991 <brettl1991@gmail.com>
2022-08-10 13:37:58 +0100

    Use react router to render out our new pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+//Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,9 @@
+const ContactUs = () => {
+  return (
+    <div>
+      <h1>Contuct Us</h1>
+    </div>
+  );
+};
+
+export default ContactUs;
