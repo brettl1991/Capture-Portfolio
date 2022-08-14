6f62d7ad51f92f65d61f7f52d6e1f50c205a31a0
brettl1991 <brettl1991@gmail.com>
2022-08-14 16:18:38 +0100

    Add scroll animation on services and faq sections

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -4,10 +4,19 @@ import { About } from "../styles";
 
 import Toggle from "./Toggle";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Faq>
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
       <h2>
         Any questions <span>FAQ</span>
       </h2>
